ac8f6552a3927142f451f7687f4447bda5a66597
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2023-06-01 23:40:16 -0500

    Word list file and Dictionary module

--- dictionary/test/dictionary_test.exs
+++ dictionary/test/dictionary_test.exs
@@ -2,7 +2,7 @@ defmodule DictionaryTest do
   use ExUnit.Case
   doctest Dictionary
 
-  test "greets the world" do
-    assert Dictionary.hello() == :world
+  test "random_word returns a string" do
+    assert Dictionary.random_word |> is_binary()
   end
 end
