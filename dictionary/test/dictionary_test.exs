2af90d985fefaeed7ace695612812d4c5e76b6f5
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2023-06-02 21:43:11 -0500

    Refactor Dictionary to separate implementation from API

--- dictionary/test/dictionary_test.exs
+++ dictionary/test/dictionary_test.exs
@@ -2,7 +2,15 @@ defmodule DictionaryTest do
   use ExUnit.Case
   doctest Dictionary
 
+  test "start returns a word list" do
+    words = Dictionary.start()
+    assert words |> is_list()
+    [head | _tail] = words
+    assert head |> is_binary()
+  end
+
   test "random_word returns a string" do
-    assert Dictionary.random_word |> is_binary()
+    words = Dictionary.start()
+    assert Dictionary.random_word(words) |> is_binary()
   end
 end
