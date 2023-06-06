ff3fe9940e0c2352e4b703a20d7303435018cfbf
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2023-06-05 23:50:24 -0500

    Convert Dictionary to use application and supervisor

---
+++ dictionary/lib/runtime/server.ex
@@ -0,0 +1,20 @@
+defmodule Dictionary.Runtime.Server do
+  @type t :: pid()
+  @me __MODULE__
+
+  use Agent
+
+  alias Dictionary.Impl.WordList
+
+  def start_link(_args) do
+    Agent.start_link(&WordList.word_list/0, name: @me)
+  end
+
+  def random_word() do
+    # temporary way to crash the dictionary process occasionally
+    if :rand.uniform() < 0.333 do
+      Agent.get(@me, fn _ -> exit(:boom) end)
+    end
+    Agent.get(@me, &WordList.random_word/1)
+  end
+end
